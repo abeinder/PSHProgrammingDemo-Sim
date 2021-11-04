a472e85e3e4a2ac9731ee7cdf07213ff74d2c74a
Austin <abeinder@iastate.edu>
2021-11-04 15:21:54 -0500

    Disabled Extraneous Op Modes

--- TeamCode/src/org/firstinspires/ftc/teamcode/MecanumDemo.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/MecanumDemo.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.*;
  *
  */
 @TeleOp(name = "mecanum bot demo", group = "MecanumBot")
+@Disabled
 public class MecanumDemo extends LinearOpMode {
 
     public void runOpMode(){
--- TeamCode/src/org/firstinspires/ftc/teamcode/TestOdom.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/TestOdom.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,6 +11,7 @@ import com.qualcomm.robotcore.hardware.DcMotorExImpl;
  * either the MecBot or the XDriveBot robot configuration.
  */
 @TeleOp(name = "TestOdom", group = "OdomBot")
+@Disabled
 public class TestOdom extends LinearOpMode {
 
     EncBot bot = new EncBot();
